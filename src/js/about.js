ac0a13454c345dc1562b4e496e2796def1034201
schooolman <j.schoolmee@gmail.com>
2016-09-15 23:26:48 -0500

    Add and connected scrolling animation on the about page will need to begin organizing information next

--- src/js/about.js
+++ src/js/about.js
@@ -3,3 +3,24 @@
  */
 
 console.log("connected");
+
+
+$(document).ready(function() {
+
+    var testHeader = $(".testHeader");
+
+    //var main = document.getElementById("mainContainer");
+
+    document.getElementById("mainContainer").onscroll = function(){
+        console.log("scrolling");
+        firstline();
+    };
+
+
+    function firstline(){
+
+        TweenMax.to(testHeader, 0.5, {x: 500, scale: 1.5});
+
+    };
+
+});
