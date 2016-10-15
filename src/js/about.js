77ae08d170a1338c73944de5b13a13a8f874074e
schooolman <j.schoolmee@gmail.com>
2016-10-14 22:04:37 -0500

    refactoring initial animation to use the jquery .scrollTop() event listener

--- src/js/about.js
+++ src/js/about.js
@@ -4,23 +4,36 @@
 $(document).ready(function() {
 
 
+    $(window).scroll(function(){
+       if($(this).scrollTop() > 60) {
+           $('.logoMain').css('right', 2000);
+           console.log('working');
+       }
+    });
+
+    //if($(window).scrollTop() > 60) {
+    //    $('.logoMain').css("right", 200);
+    //    console.log('working');
+    //}
+
+
     //Animating CSS with jquery
     //Logo moves from right to left on scroll
     var scrollCounter = 0;
     var i = 0;
 
-    if(i < 100){
-        $(window).scroll(function(){
-            scrollCounter += 10;
-            i += 5;
-            $(".logoMain").css("right", scrollCounter);
-            console.log(scrollCounter);
-            //return scrollCounter;
-        });
-        // moveLogo();
-    } else {
-        fadeLogo();
-    }
+    //if(scrollCounter < 100){
+    //    $(window).scroll(function(){
+    //        scrollCounter += 10;
+    //        i += 5;
+    //        $(".logoMain").css("right", scrollCounter);
+    //        console.log(scrollCounter);
+    //        //return scrollCounter;
+    //    });
+    //    // moveLogo();
+    //} else {
+    //    fadeLogo();
+    //}
 
     function moveLogo(){
         $(window).scroll(function(){
