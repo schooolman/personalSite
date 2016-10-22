a57aa8e6887ae9d44e39add028b197c9b0533ea9
schooolman <j.schoolmee@gmail.com>
2016-10-21 19:46:14 -0500

    Currently in the car with no internet connection. Cleaned up the about.js file and organized the code a bit.

--- src/js/about.js
+++ src/js/about.js
@@ -3,29 +3,29 @@
 
 $(document).ready(function() {
 
+    // Global Variables declared here
     var headerOne = $(".sectionOneHeader");
     var headerTwo = $(".sectionOneSubHeader");
     var planeOne = $(".airplane");
     var planeTwo = $(".airplaneTwo");
     var planeThree = $(".airplaneThree");
 
-    var tl = new TimelineMax();
-
     //This time line fires on page load. Use callbacks for more animations
     //
+    var tl = new TimelineMax();
     tl.to(headerOne, 1, {scale: 1.2})
         .to(headerTwo, 1, {scale: 1.1});
 
-
-
-
+    //----------------------------------------------------//
+    //----------------------------------------------------//
+    //------Animations triggered on scroll functions------//
+    //----------------------------------------------------//
+    //----------------------------------------------------//
     $(window).scroll(function(){
         var logoMove = $(this).scrollTop();
 
        if($(this).scrollTop() < 1200) {
-           //$('.logoMain').css('right', 1500);
            $('.logoMain').css('right', logoMove);
-           //console.log('working');
        }
 
         if($(this).scrollTop() > 1000) {
@@ -41,49 +41,12 @@ $(document).ready(function() {
         }
     });
 
-    //if($(window).scrollTop() > 60) {
-    //    $('.logoMain').css("right", 200);
-    //    console.log('working');
-    //}
 
-
-
-    //This is the first attempt at using jquery scroll event for animation
-    //--------DELETE BELOW EVENTUALLY---------//
-    //Animating CSS with jquery
-    //Logo moves from right to left on scroll
-    var scrollCounter = 0;
-    var i = 0;
-
-    //if(scrollCounter < 100){
-    //    $(window).scroll(function(){
-    //        scrollCounter += 10;
-    //        i += 5;
-    //        $(".logoMain").css("right", scrollCounter);
-    //        console.log(scrollCounter);
-    //        //return scrollCounter;
-    //    });
-    //    // moveLogo();
-    //} else {
-    //    fadeLogo();
-    //}
-
-    function moveLogo(){
-        $(window).scroll(function(){
-            scrollCounter++;
-            $(".logoMain").css("right", (scrollCounter + 5));
-            console.log(scrollCounter);
-            return scrollCounter;
-        });
-    }
-
-    function fadeLogo(){
-        $(".logoMain").css("opacity", 0);
-        console.log("else!");
-        // alert("elseStatement!");
-    }
-
-    // function for the timeline of the airplane animations.
+    //-----------------------------------------------------//
+    //-----------------------------------------------------//
+    //------Airplane animations for content section 3------//
+    //-----------------------------------------------------//
+    //-----------------------------------------------------//
     function getPlaneTimeline() {
         var planeTimeline = new TimelineMax();
 
@@ -98,5 +61,18 @@ $(document).ready(function() {
             .to(planeTwo, 1.5, {bottom: 300, left: 1200});
     }
 
+    function moveLogo(){
+        $(window).scroll(function(){
+            scrollCounter++;
+            $(".logoMain").css("right", (scrollCounter + 5));
+            console.log(scrollCounter);
+            return scrollCounter;
+        });
+    }
+
+    function fadeLogo(){
+        $(".logoMain").css("opacity", 0);
+        console.log("else!");
+    }
 
 });
